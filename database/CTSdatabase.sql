d4adbf21a85505b537291a97e2303199d28180ef
Moskoskos <atleag@outlook.com>
2015-04-12 15:48:05 +0200

    Updated database

--- database/CTSdatabase.sql
+++ database/CTSdatabase.sql
@@ -9,11 +9,57 @@
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
--- Data exporting was unselected.
+
+-- Dumping database structure for cts
+CREATE DATABASE IF NOT EXISTS `cts` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `cts`;
+
+
+-- Dumping structure for table cts.alarm_historian
+CREATE TABLE IF NOT EXISTS `alarm_historian` (
+  `alarm_event_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `datetime_recorded` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `alarm_id` int(11) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`alarm_event_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 
+
+-- Dumping structure for table cts.historian
+CREATE TABLE IF NOT EXISTS `historian` (
+  `historian_id` int(11) NOT NULL AUTO_INCREMENT,
+  `datetime_recorded` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `value` double DEFAULT NULL,
+  PRIMARY KEY (`historian_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- Data exporting was unselected.
+
+
+-- Dumping structure for table cts.settings
+CREATE TABLE IF NOT EXISTS `settings` (
+  `settings_id` int(11) NOT NULL AUTO_INCREMENT,
+  `setpoint_low` double NOT NULL,
+  `setpoint_high` double NOT NULL,
+  `hysteresis` double NOT NULL,
+  PRIMARY KEY (`settings_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table cts.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` varchar(45) NOT NULL,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `phonenumber` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`username`),
+  UNIQUE KEY `userID_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
